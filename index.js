56c8996698236e941395eb0dadad0fffad2c1e08
Rene0115 <enere0115@gmail.com>
2022-06-20 03:51:26 +0100

    first weather project commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const https = require('https');
+
+const app = express();
+
+
+app.get("/", function(req, res){
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=690c09bd3b51457e02d419b1ed0e072d"
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+       res.send("The temperature in londo is " + temp + "degrees Celcius.");
+    })
+  })
+
+  res.send("Server dey work")
+});
+
+
+
+
+app.listen (5000, function(){
+  console.log ("Server is running on port 5000" )
+});
